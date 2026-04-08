6b5ebf3c804d911cf4777a3addd00c04ff0de584
Dzobanana-2008 <vasamioc2008@gmail.com>
2026-04-08 03:59:22 -0700

    hrfuiwrhj

--- program.c
+++ program.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
 int main(){
+  int x,y,z;
   printf("Ovde bi trebalo da je nesto :/");
-
+  printf("Ovde bi trebalo da je nesto :/");
+  x=5;
+  y=2;
+  z=x-y;
+  printf("%d",z);
   return 0;
 }
