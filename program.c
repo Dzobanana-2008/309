44787742bfb6505e47c8568e065b69d33c6af540
Dzobanana-2008 <vasamioc2008@gmail.com>
2026-04-09 03:09:36 -0700

    ovo je dobra izmena

--- program.c
+++ program.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(){
-  int x,y,z;
+  int x,y,z,a;
   printf("Ovde bi trebalo da je nesto :/");
   printf("Ovde bi trebalo da je nesto :/");
-  x=5;
-  y=2;
-  z=x-y;
-  printf("%d",z);
+  x=9;
+  y=1;
+  a=x+y;
+  printf("%d",y);
   return 0;
 }
